0550db2a90aac23a5c2cab4d56a2711ec8d370c0
Joseph Burks <joe_burks_3@yahoo.com>
2019-10-26 23:53:19 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,24 @@
-# Your Code Here
+def map(array)
+  new = []
+  i = 0
+  while i < array.length
+    new << (yield(array[i]))
+    i += 1
+  end
+  new
+end
+
+def reduce(array, start_point=nil)
+  if start_point
+    total = start_point
+  else
+    total = 0
+  end
+  i = 0
+  while i < array.length
+    total = yield(total, array[i])
+    i += 1
+  end
+  total
+end
+  
